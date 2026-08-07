415980147e8e243a99a086f349834a76f3eb1930
Anees6 <140949830+Anees6@users.noreply.github.com>
2026-08-07 08:37:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
-TOKEN = "8673412670:AAGCYcqdMWXXnR73MmtEMGIaPR06d2aDZNw"
+TOKEN = "8673412670:AAGd09Zfk-RSY83drPF01ZJqniJkrLWDJh4"
 
 # പഴയ മെസ്സേജുകളുടെ ഐഡി സൂക്ഷിക്കാനുള്ള ഗ്ലോബൽ വേരിയബിളുകൾ
 last_warning_message_id = {} # {chat_id: message_id}
